d49ff43e0fa16379d9b4402ed5279091158951d0
Rakgadib <b.rankapule@gmail.com>
2023-02-21 08:43:42 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
+/**
+ * main - starting point
+ *
+ * Return: 0 success
+ */
+
 #include "main.h"
 
-/**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char *c = "_putchar";
 
+	while (*c != '\0')
+	{
+		_putchar(*c);
+	}
+	_putchar('\n');
 	return (0);
 }
